d55e7d781c30f1d87f666bb7fce4008645bf70be
Mohamed Sarhan <mohamed.sarhan123789@gamil.com>
2023-08-12 01:39:51 +0300

    n

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the alphaabets
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		puttcher(ch);
+		ch++;
+	}
+	putcher('\n');
+
+	return (0);
