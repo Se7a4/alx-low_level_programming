694687ed4a4f55dabe751765888cbe94ef168b30
Se7a4 <mohamed.sarhan123789@gamil.com>
2023-09-21 22:54:13 +0300

    12

---
+++ .test/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing some shit in low level lang
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ .test/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: this code prints out some pullshit
+ *
+ * Return: always (0)
+*/
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is piece of art,\n");
+	return (0);
+}
+
