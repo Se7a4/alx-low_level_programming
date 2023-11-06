abfa3938d004727eaf2aa184c755d46a4b89760a
Se7a4 <mohamed.sarhan123789@gamil.com>
2023-11-06 16:29:16 +0200

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,30 +2,31 @@
 /**
  * print_diagonal- function that draws a diagonal line on the terminal.
  *
+ * Description: printing digonal line.
+ * @n: the numper of the diagonal slashes
  * returns: always 0.
- * 
 */
 
 void print_diagonal(int n)
 {
-int slash =0;
-int space =0;
-    if (n < 1)
-    {
-        _putchar('\n');
-    }
-    else
-    while (slash < n)
-        {
-            space=0;
-            while (space < slash)
-            {
-                _putchar(' ');
-                space++;
-            }
-            _putchar(92);
-            _putchar('\n');
-             slash++;
-        }
+int slash = 0;
+int space = 0;
+if (n < 1)
+{
+_putchar('\n');
+}
+else
+while (slash < n)
+{
+space = 0;
+while (space < slash)
+{
+_putchar(' ');
+space++;
+}
+_putchar(92);
+_putchar('\n');
+slash++;
+}
 }
 
