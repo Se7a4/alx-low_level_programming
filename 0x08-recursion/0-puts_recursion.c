3034fbfdc01b868fe6808917e3a0df6a52456761
Se7a4 <mohamed.sarhan123789@gmail.com>
2023-12-10 10:54:42 +0200

    puts-r

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,14 +9,15 @@
  * Return: Always 0.
 */
 
+
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
+if (*s)
 {
-	_putchar('\n');
-	return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
-_putchar(*s);
-_puts_recursion(s + 1);
+else
+_putchar('\n');
 }
 
