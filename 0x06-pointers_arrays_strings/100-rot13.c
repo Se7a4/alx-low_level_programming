c65f86e174783abcef13cdf9b18c1c7feefaac2a
mohamed sarhan <mohamed.sarhan123789@gmail.com>
2025-08-31 18:30:24 +0300

    rot13 ADV

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* rot13 - rotates the letters by 13 index.
+* @s : The parameter Pointer to string
+* return : The string s
+*/
+
+char *rot13(char *s)
+{
+int  i = 0;
+int j;
+for (i = 0; s[i] != '\0'; i++)
+{
+if ((s[i] > 109 && s[i] <= 122) || (s[i] > 77 && s[i] <= 90))
+{
+j = *(s + i);
+j -= 13;
+*(s + i) = j;
+}
+else if ((s[i] <= 109 && s[i] >= 97) || (s[i] <= 77 && s[i] >= 65))
+{
+j = *(s + i);
+j += 13;
+*(s + i) = j;
+}
+}
+return (s);
+}
