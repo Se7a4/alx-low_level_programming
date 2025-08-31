9e39b6d59dba74ab723b316820690eb6e994b2f1
mohamed sarhan <mohamed.sarhan123789@gmail.com>
2025-08-31 21:46:17 +0300

    magicXD

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*
+ */
+
+void print_number(int n)
+{
+int *ptr = &n;
+char num[100] = *ptr;
+
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef INV_HOLBERTON_H
-#define INV_HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char);
 char *_strcat(char *dest, char *src);
@@ -11,5 +11,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
 
 #endif
